6b8af72b91a0d2ca6810a7c086de1a140603acc2
Kate <vvellspring@gmail.com>
2022-09-13 19:47:12 +0300

    Задача 47: заполнение двумерного массива вещественными числами

---
+++ Task47/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+Console.Clear();
+int Prompt(string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+double[,] FillArray(int numLine, int NumColumns)
+{
+    double[,] matrix = new double[numLine, NumColumns];
+    for (int i = 0; i < matrix.GetLength(0); i++)     // Строка
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // Столбец
+        {
+            matrix[i,j] = new Random().Next(0,10);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+     for (int j = 0; j < matrix.GetLength(1); j++)
+     {
+        Console.Write($"{matrix[i,j]}\t");
+     }   
+     Console.WriteLine();
+    }
+}
+
+
+double[,] myArray = FillArray(Prompt("Введите m: "),Prompt("Введите n: "));
+PrintArray(myArray);
